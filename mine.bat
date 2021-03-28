5f6e55ef04cba1d2d4ab6c0a43432b17b3396a22
Per Ivar Nerseth <perivar@nerseth.com>
2021-03-28 18:58:08 +0200

    added docker and conf files

---
+++ mine.bat
@@ -0,0 +1,4 @@
+echo Mining... Press [CTRL+C] to stop
+:loop
+	niftycoin-cli.exe generate 1
+goto loop
---
+++ mine.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Mining... Press [CTRL+C] to stop"
+while :
+do
+  src/niftycoin-cli generate 1
+done
