1c3875e0b737c0fbb5e7792f1aa5fa0ec583ff59
Per Ivar Nerseth <perivar@nerseth.com>
2021-05-09 20:08:58 +0200

    reverted the validation.cpp segwit changes

--- src/validation.cpp
+++ src/validation.cpp
@@ -3147,7 +3147,8 @@ bool CheckBlock(const CBlock& block, CValidationState& state, const Consensus::P
 bool IsWitnessEnabled(const CBlockIndex* pindexPrev, const Consensus::Params& params)
 {
     // PIN- transactions stuck in mempool - make sure Segwit always are enabled
-    return true;
+    // Reverted and commented out
+    // return true;
     LOCK(cs_main);
     return (VersionBitsState(pindexPrev, params, Consensus::DEPLOYMENT_SEGWIT, versionbitscache) == ThresholdState::ACTIVE);
 }
@@ -3155,7 +3156,8 @@ bool IsWitnessEnabled(const CBlockIndex* pindexPrev, const Consensus::Params& pa
 bool IsNullDummyEnabled(const CBlockIndex* pindexPrev, const Consensus::Params& params)
 {
     // PIN- transactions stuck in mempool - make sure Segwit always are enabled
-    return true;
+    // Reverted and commented out
+    // return true;
     LOCK(cs_main);
     return (VersionBitsState(pindexPrev, params, Consensus::DEPLOYMENT_SEGWIT, versionbitscache) == ThresholdState::ACTIVE);
 }
@@ -3302,7 +3304,8 @@ static bool ContextualCheckBlock(const CBlock& block, CValidationState& state, c
         if (block.vtx[0]->vin[0].scriptSig.size() < expect.size() ||
             !std::equal(expect.begin(), expect.end(), block.vtx[0]->vin[0].scriptSig.begin())) {
             // PIN - make sure segwit if always enabled, disabled the return below
-            // return state.DoS(100, false, REJECT_INVALID, "bad-cb-height", false, "block height mismatch in coinbase");
+            // Reverted this change and enabled again
+            return state.DoS(100, false, REJECT_INVALID, "bad-cb-height", false, "block height mismatch in coinbase");
         }
     }
 
